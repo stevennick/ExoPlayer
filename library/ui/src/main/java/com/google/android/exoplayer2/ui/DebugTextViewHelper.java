8daf6c6d90f7a667e9544f0295ec6926b51c44e4
Stevennick <stevennick@gmail.com>
2017-06-09 17:12:42 +0800

    1. Make system status bar hide during playback 2. Use custom LoadControl for controllable buffer size 3. Modify debug window messages for more clear understanding

--- library/ui/src/main/java/com/google/android/exoplayer2/ui/DebugTextViewHelper.java
+++ library/ui/src/main/java/com/google/android/exoplayer2/ui/DebugTextViewHelper.java
@@ -178,10 +178,10 @@ public final class DebugTextViewHelper implements Runnable, ExoPlayer.EventListe
       return "";
     }
     counters.ensureUpdated();
-    return " rb:" + counters.renderedOutputBufferCount
-        + " sb:" + counters.skippedOutputBufferCount
-        + " db:" + counters.droppedOutputBufferCount
-        + " mcdb:" + counters.maxConsecutiveDroppedOutputBufferCount;
+    return " rendbuf:" + counters.renderedOutputBufferCount
+        + " skipbuf:" + counters.skippedOutputBufferCount
+        + " dopbuf:" + counters.droppedOutputBufferCount
+        + " maxcondropbuf:" + counters.maxConsecutiveDroppedOutputBufferCount;
   }
 
 }
