d31d7efb187d2d15f7981b37f230ba6f892595a4
Stevennick <stevennick@gmail.com>
2017-06-09 14:22:13 +0800

    Extend demo application to accept localhost UDP stream.

--- demo/src/main/java/com/google/android/exoplayer2/demo/DemoApplication.java
+++ demo/src/main/java/com/google/android/exoplayer2/demo/DemoApplication.java
@@ -16,11 +16,18 @@
 package com.google.android.exoplayer2.demo;
 
 import android.app.Application;
+import android.net.Uri;
+
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.upstream.UdpDataSource;
+import com.google.android.exoplayer2.upstream.cache.Cache;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory;
+import com.google.android.exoplayer2.upstream.cache.SimpleCache;
 import com.google.android.exoplayer2.util.Util;
 
 /**
@@ -37,16 +44,52 @@ public class DemoApplication extends Application {
   }
 
   public DataSource.Factory buildDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
-    return new DefaultDataSourceFactory(this, bandwidthMeter,
-        buildHttpDataSourceFactory(bandwidthMeter));
+    return buildDataSourceFactory(Uri.parse("http://127.0.0.1"), bandwidthMeter );
+  }
+
+  public DataSource.Factory buildDataSourceFactory(Uri uri, DefaultBandwidthMeter bandwidthMeter) {
+    DataSource.Factory dataSourceFactory;
+    switch(uri.getScheme()) {
+      case "udp":
+        dataSourceFactory = new DefaultDataSourceFactory(this, bandwidthMeter,
+                buildUdpDataSourceFactory(bandwidthMeter));
+        break;
+      case "http":
+      default:
+        dataSourceFactory = new DefaultDataSourceFactory(this, bandwidthMeter,
+                buildHttpDataSourceFactory(bandwidthMeter));
+        break;
+    }
+    return dataSourceFactory;
   }
 
   public HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
     return new DefaultHttpDataSourceFactory(userAgent, bandwidthMeter);
   }
 
+  public UdpDataSource.Factory buildUdpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+    return new DefaultDataSourceFactory(this, bandwidthMeter, new UdpDataSourceFactory(bandwidthMeter));
+  }
+
   public boolean useExtensionRenderers() {
     return BuildConfig.FLAVOR.equals("withExtensions");
+  }
+
+  /**
+   * Class UdpDataSourceFactory for UDP data source.
+   */
+  public class UdpDataSourceFactory implements DataSource.Factory {
+
+    private DefaultBandwidthMeter bandwidthMeter;
+
+    public UdpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+      this.bandwidthMeter = bandwidthMeter;
+    }
+
+    @Override
+    public DataSource createDataSource() {
+      return new UdpDataSource(bandwidthMeter);
+    }
   }
 
 }
